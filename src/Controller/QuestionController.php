a6cd511af632e67828e658468dbbdaa29a69fdd0
Silviu <silviu85@users.noreply.github.com>
2021-11-19 13:27:26 +0200

    Adding ajax and others

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,22 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/",name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
-        return new Response("What the fuck is talking this?");
+        /** 
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+        return new Response($html);
+        */
+        return $this->render('question/homepage.html.twig');
     }
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}",name="app_question_show")
      */
     public function show($slug)
     {
         $answers = ['This is a test', 'This is a dummy answer', 'This is done'];
+
+        dump($this);
+
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
